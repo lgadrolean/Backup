35aa6741e423b986c8be3d9558609e2525857c4b
Leandro Avila <lga.drolean@gmail.com>
2025-02-18 20:50:11 -0300

    update

--- functions.py
+++ functions.py
@@ -12,8 +12,8 @@ def copytree(src, dst, log_path):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
-        if '.git' in s.split(os.sep):
-            log_message(log_path, f"Ignoring the folder: {s}")
+        if any(ignored in s.split(os.sep) for ignored in ['.git', '__pycache__']):
+            log_message(log_path, f"Ignorando a pasta: {s}")
             continue
         try:
             if os.path.isdir(s):
@@ -21,7 +21,7 @@ def copytree(src, dst, log_path):
             else:
                 shutil.copy2(s, d)
         except Exception as e:
-            log_message(log_path, f"Error copying {s} to {d}: {e}")
+            log_message(log_path, f"Erro ao copiar {s} para {d}: {e}")
 
 def create_backup(source_path, hot_backup_path, daily_backup_path, backupname, log_path, onedrive_path, keep_count_cloud):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M')
@@ -29,11 +29,11 @@ def create_backup(source_path, hot_backup_path, daily_backup_path, backupname, l
     backup_path = os.path.join(hot_backup_path, backup_name)
     
     log_message(log_path, "###########################################")
-    log_message(log_path, "Starting the backup process.")
+    log_message(log_path, "Iniciando o processo de backup.")
     
     temp_path = os.path.join(hot_backup_path, "temp_backup")
     copytree(source_path, temp_path, log_path)
-    log_message(log_path, f"Folder copied from {source_path} to {temp_path}.")
+    log_message(log_path, f"Pasta copiada de {source_path} para {temp_path}.")
 
     with zipfile.ZipFile(backup_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(temp_path):
@@ -43,21 +43,21 @@ def create_backup(source_path, hot_backup_path, daily_backup_path, backupname, l
                 try:
                     zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), temp_path))
                 except Exception as e:
-                    log_message(log_path, f"Error compressing {file}: {e}")
-    log_message(log_path, f"Folder compressed into {backup_path}.")
+                    log_message(log_path, f"Erro ao comprimir {file}: {e}")
+    log_message(log_path, f"Pasta comprimida em {backup_path}.")
     shutil.rmtree(temp_path)
-    log_message(log_path, f"Temporary folder {temp_path} removed.")
+    log_message(log_path, f"Pasta temporária {temp_path} removida.")
 
     daily_backup_name = f"{backupname}_{datetime.now().strftime('%Y%m%d')}.zip"
     daily_backup_path_full = os.path.join(daily_backup_path, daily_backup_name)
     if not os.path.exists(daily_backup_path_full):
         shutil.copy2(backup_path, daily_backup_path_full)
-        log_message(log_path, f"Daily backup moved to {daily_backup_path_full}.")
+        log_message(log_path, f"Backup diário movido para {daily_backup_path_full}.")
 
     onedrive_backup_path_full = os.path.join(onedrive_path, daily_backup_name)
     if not os.path.exists(onedrive_backup_path_full):
         shutil.copy2(backup_path, onedrive_backup_path_full)
-        log_message(log_path, f"Daily backup moved to OneDrive: {onedrive_backup_path_full}.")
+        log_message(log_path, f"Backup diário movido para o OneDrive: {onedrive_backup_path_full}.")
 
     maintain_backups(onedrive_path, backupname, log_path, keep_count_cloud, "cloud")
 
@@ -65,17 +65,17 @@ def create_backup(source_path, hot_backup_path, daily_backup_path, backupname, l
     log_backup_statistics(daily_backup_path, hot_backup_path, onedrive_path, backupname, log_path)
 
 def maintain_backups(backup_path, backupname, log_path, keep_count, tipo="local"):
-    log_message(log_path, f"Checking old backups for removal in {backup_path}.")
+    log_message(log_path, f"Verificando backups antigos para remoção em {backup_path}.")
     backups = sorted([f for f in os.listdir(backup_path) if f.startswith(backupname) and f.endswith(".zip")], key=lambda x: os.path.getmtime(os.path.join(backup_path, x)), reverse=True)
     for backup in backups[keep_count:]:
         os.remove(os.path.join(backup_path, backup))
-        log_message(log_path, f"Old {tipo} backup removed: {backup}")
+        log_message(log_path, f"Backup {tipo} antigo removido: {backup}")
 
 def log_backup_statistics(daily_backup_path, hot_backup_path, onedrive_path, backupname, log_path):
     daily_backups = [f for f in os.listdir(daily_backup_path) if f.startswith(backupname) and f.endswith(".zip")]
     hot_backups = [f for f in os.listdir(hot_backup_path) if f.startswith(backupname) and f.endswith(".zip")]
     cloud_backups = [f for f in os.listdir(onedrive_path) if f.startswith(backupname) and f.endswith(".zip")]
 
-    log_message(log_path, f"Number of daily backups: {len(daily_backups)}")
-    log_message(log_path, f"Number of hot backups: {len(hot_backups)}")
-    log_message(log_path, f"Number of cloud backups: {len(cloud_backups)}")
+    log_message(log_path, f"Número de backups diários: {len(daily_backups)}")
+    log_message(log_path, f"Número de backups quentes: {len(hot_backups)}")
+    log_message(log_path, f"Número de backups na nuvem: {len(cloud_backups)}")
